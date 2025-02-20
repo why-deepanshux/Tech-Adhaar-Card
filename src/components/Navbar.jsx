b419373321237e138b448d80f0fd8b06414710ca
why-deepanshux <deepanshusharma12800@gmail.com>
2025-02-20 22:31:49 +0530

    github linked in links updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="w-full shadow-md bg-white sticky top-0 z-50">
       <div className="md:w-[90%] h-[10vh] mx-auto w-full py-4 flex flex-row justify-between items-center">
         <div className="text-2xl font-semibold">Deepanshu Sharma</div>
-        <div className="flex flex-row items-center gap-[1.5rem] w-[30%] justify-between text-lg">
+        <div className=" flex flex-row items-center gap-[1.5rem] w-[30%] justify-between text-lg">
           <a href="#home">
             <div>Home</div>
           </a>
@@ -24,16 +24,21 @@ const Navbar = () => {
         </div>
 
         <div className="flex flex-row gap-[1rem] items-center">
-          <div className="h-[40px] w-[40px]  rounded-full">
-            <img src={github} alt="github icon" className="w-full" />
-          </div>
-          <div className="h-[40px] w-[40px] rounded-full">
-            <img
-              src={linkedIn}
-              alt="Linked In Logo"
-              className="h-full w-full"
-            />
-          </div>
+          <a href="https://github.com/why-deepanshux">
+            <div className="h-[40px] w-[40px]  rounded-full">
+              <img src={github} alt="github icon" className="w-full" />
+            </div>
+          </a>
+
+          <a href="https://www.linkedin.com/in/deepanshu-sharma-449b661ba/">
+            <div className="h-[40px] w-[40px] rounded-full">
+              <img
+                src={linkedIn}
+                alt="Linked In Logo"
+                className="h-full w-full"
+              />
+            </div>
+          </a>
           <div className="h-[40px] w-[40px] rounded-full bg-black">
             {/* <img src={twitter} alt="x.logo" className="h-full w-full" /> */}
           </div>
