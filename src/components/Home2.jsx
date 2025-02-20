69ded5d9da9e40752ca27a72b8199609ab0e7725
why-deepanshux <deepanshusharma12800@gmail.com>
2025-02-20 20:57:17 +0530

    resume link updated

--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import main from "../assets/main.mp4"
 const Home2 = () => {
   return (
-    <div id="home" className="w-[90%] mt-4 h-[90vh] flex md:flex-row flex-col-reverse justify-between mx-auto">
+    <div
+      id="home"
+      className="w-[90%] mt-4 h-[90vh] flex md:flex-row flex-col-reverse justify-between mx-auto"
+    >
       <div className="w-full md:w-[45%] flex flex-col justify-center px-2 ">
         <div className="text-4xl font-semibold">Hello</div>
         <div className="text-4xl font-semibold">
@@ -12,7 +15,7 @@ const Home2 = () => {
           A Software Engineer and Web Developer
         </div>
         <a
-          href="https://drive.google.com/file/d/1in9ZSGt0uJKJQpPvOIf3M0wdJjhtYCIc/view?usp=sharing"
+          href="https://drive.google.com/file/d/1HURLDHKJhTXDBm9IczTsN6kGGOch8m_J/view?usp=sharing"
           target="_blank"
         >
           <button className=" px-8 py-2  bg-[var(--primary-color)] text-[var(--background-color)] mt-1 text-lg cursor-pointer">
