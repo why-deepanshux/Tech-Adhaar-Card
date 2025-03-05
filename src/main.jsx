afdfce8efe75b333a9160ab4381063e11cdd2af4
why-deepanshux <deepanshusharma12800@gmail.com>
2025-03-05 23:27:43 +0530

    web analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App.jsx";
-
+import { Analytics } from "@vercel/analytics/next";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
