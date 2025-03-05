6163471302c426f283c14a7937ba430ca6a62b12
why-deepanshux <deepanshusharma12800@gmail.com>
2025-03-05 23:32:17 +0530

    analytics fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/next";
+import { inject } from "@vercel/analytics";
+
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
